cc55eb0425d294079905ad98ef788dc5c4079136
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-03-29 02:17:59 +0500

    update classes

--- src/ClassInterfacesAndObjectOrientedProgramming/Classes.ts
+++ src/ClassInterfacesAndObjectOrientedProgramming/Classes.ts
@@ -1,1 +1,29 @@
 // Classes 101
+type Base = 'classic' | 'thin' | 'thick' | 'garlic'
+
+class Pizza {
+    constructor(title: string, price: number) {
+        this.title = title
+        this.price = price
+
+    }
+    title: string
+    price: number
+    base: Base = 'classic'
+    toppings: string[] = []
+
+    addTopping(topping: string): void {
+        this.toppings.push(topping)
+    }
+    removeTopping(topping: string): void {
+        this.toppings = this.toppings.filter((t) => t !== topping)
+    }
+    selectBase(b: Base): void {
+        this.base = b
+    }
+}
+const pizza = new Pizza('Cheeious Special', 1400)
+pizza.addTopping('Olive')
+pizza.addTopping('Cream')
+pizza.selectBase("thick")
+console.log('Pizza', pizza)
